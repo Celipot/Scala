14973d41482019a22e409cf44288e7d465068df6
Tang Jérémy <jeremy.tang@efrei.net>
2022-01-31 15:20:12 +0100

    test scala

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -1,9 +1,34 @@
+import com.rocketfuel.sdbc.PostgreSql._
 import parseurNoRegex._
+import Airport._
+import Country._
+import Runway._
+
 import org.scalatest.FunSuite
- 
+
  class CubeCalculatorTest extends FunSuite {
-   test("assert") {
-     val p = Point(2,1)
-     assert(Point.pointFromLine(Array("2","1"))==Right(Point(2,1)))
+  test("d'un test") {
+     assert(Math.pow(3,3) === 27)
    }
+
+  test("select"){
+    val conn = Connection.using("jdbc:postgresql://localhost:5432/test?user=postgres&password=password"){implicit connection =>
+    Select.iterator[Country]("select * from country Where ID = 302672"  /*"select * from country Where name like 'Aus%'"*/).foreach{line=>
+      assert(line == Country(302672,"AD","Andorra"))
+     }
+    }
+   }
+  test("insert and creation"){
+    val conn = Connection.using("jdbc:postgresql://localhost:5432/test?user=postgres&password=password"){implicit connection =>
+    Ignore.ignore("DROP TABLE IF EXISTS TestC")
+    Ignore.ignore("CREATE TABLE TestC (id int PRIMARY KEY,code varchar(255), name varchar(255))")
+    val request = (s"INSERT INTO TestC (id,code,name) VALUES (302672,'AD','Andorra')");
+    Ignore.ignore(request)  
+    Select.iterator[Country]("select * from TestC Where ID = 302672"  /*"select * from country Where name like 'Aus%'"*/).foreach{line=>
+      assert(line == Country(302672,"AD","Andorra"))
+     }
+    }
+   }
+
+
  }
